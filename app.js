9e7358f917a5fc94f880163b0e620a3c757c4fdf
Stanton Huang <stantonhuang11@gmail.com>
2019-10-31 13:14:00 -0700

    adds validator and model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const products = require("./routes/api/products");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -9,6 +10,10 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("fuck mern"));
+app.use("/api/products", products);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
---
+++ models/Product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: false
+    }
+})
+
+module.exports = Product = mongoose.model('products', ProductSchema);
