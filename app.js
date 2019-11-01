18914d956e7297785f28a0a7adcecbadc4c25046
Stanton Huang <stantonhuang11@gmail.com>
2019-11-01 10:43:29 -0700

    heroku setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const products = require("./routes/api/products");
+const path = require('path');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +13,13 @@ mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 app.get("/", (req, res) => res.send("fuck mern"));
 app.use("/api/products", products);
